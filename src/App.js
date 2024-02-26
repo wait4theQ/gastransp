0c2c8f5ef97957f17aa37823335d485af7033e6a
wait4theQ <waitforthekyu@gmail.com>
2024-02-26 17:20:43 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background-image">
+        <div className="content">
+          <h1 className="title">Bem-vindo a GAS TRANSPORTES</h1>
+          <p className="text">Fale de você ou de sua empresa aqui.</p>
+          <button className="transparent-button">Conte-nos mais</button>
+        </div>
+      </div>
     </div>
   );
 }
